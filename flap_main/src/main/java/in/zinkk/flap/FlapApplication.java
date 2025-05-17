3b0f69b885e38ee38963c71e2c579ff92035b78d
Rohan Padile <roohan.padile@gmail.com>
2025-05-17 19:08:24 +0530

    initial files for flap

---
+++ flap_main/src/main/java/in/zinkk/flap/FlapApplication.java
@@ -0,0 +1,13 @@
+package in.zinkk.flap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlapApplication.class, args);
+	}
+
+}
---
+++ flap_main/src/test/java/in/zinkk/flap/FlapApplicationTests.java
@@ -0,0 +1,13 @@
+package in.zinkk.flap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
